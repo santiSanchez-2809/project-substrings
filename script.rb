ffb91b323e3ffc603391b6e31b205514a58bad0a
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-14 12:51:25 -0300

    Add function substrings

--- script.rb
+++ script.rb
@@ -1,1 +1,9 @@
-
+def substrings(str, dict)
+    str.downcase.split(/[ ,\.?!']/).reduce(Hash.new(0)) do |result, word|
+        result[word] += 1 if dict.any?{|dict_word| dict_word == word}
+        dict.each do |dict_word| 
+            result[dict_word] += 1 if word.include?(dict_word) && dict_word != word
+        end
+        result
+    end
+end
