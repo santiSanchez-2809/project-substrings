35b006ec075dc5311b688c369abb42db1242e5ee
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-14 13:16:25 -0300

    Make use of intermediate variables; replace double check with simple check within nested loop;

--- script.rb
+++ script.rb
@@ -1,9 +1,29 @@
 def substrings(str, dict)
-    str.downcase.split(/[ ,\.?!']/).reduce(Hash.new(0)) do |result, word|
-        result[word] += 1 if dict.any?{|dict_word| dict_word == word}
-        dict.each do |dict_word| 
-            result[dict_word] += 1 if word.include?(dict_word) && dict_word != word
+    words = str.downcase.split(/[\s,\.?!']+/) 
+    result = Hash.new(0)
+
+    words.each_with_object(result) do |word, result|
+        dict.each do |dict_word|
+            result[dict_word] += 1 if word.include?(dict_word)
         end
-        result
     end
+
+    #words.each do |word|
+    #  dict.each do |dict_word|
+    #    result[dict_word] += 1 if word.include?(dict_word)
+    #  end
+    #end
+
+    result
+    
+    #str.downcase.split(/[ ,\.?!']/).reduce(Hash.new(0)) do |result, word|
+    #    result[word] += 1 if dict.include?(word)
+    #
+    #   dict.each do |dict_word| 
+    #       result[dict_word] += 1 if word.include?(dict_word) && dict_word != word
+    #    end
+
+    #    result
+    #end
+    
 end
